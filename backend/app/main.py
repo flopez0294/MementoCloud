a7cba5b0edd168594c3cebfebc888e1a90c3119e
flopez0294 <flopez0294@gmail.com>
2026-07-19 12:30:58 -0700

    feat: configure CORS middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 from app.db import create_db_and_tables
 from app.routers import event
 from contextlib import asynccontextmanager
+import os
+
+load_dotenv()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -9,6 +14,16 @@ async def lifespan(app: FastAPI):
     yield
     
 app = FastAPI(lifespan=lifespan)
+
+frontend_url = os.getenv("FRONTEND_URL")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[frontend_url] if frontend_url else [],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(event.router)
 
